52095b4f9bf4645733bf7894b0ac1b4db7dda2f2
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2020-05-16 17:17:45 +0200

    Switch to legxin - First auth works

--- src/App.svelte
+++ src/App.svelte
@@ -35,127 +35,85 @@
 </style>
 
 <script>
-	import { gapi } from './gapi';
 	import { onMount } from 'svelte';
+	import snoowrap from './snoowrap-v1.min';
 	import Content from './Content.svelte';
 
-	let apiKeyInput, apiKey;
-	let clientIdInput, clientId;
-
 	let signedIn = false;
+	let waiting = false;
+	let r;
+	let hot = [];
 
 	onMount(async () => {
-		handleClientLoad()
+		// Case 1: we are in part 2 of OAuth flow
+		const code = new URL(window.location.href).searchParams.get('code')
+		// TODO: check state
+		if (code) { 
+			window.history.replaceState({}, document.title, "/");
+			const rr = await window.snoowrap.fromAuthCode({
+				code: code,
+				userAgent: 'snoowrap:github.com/rakoo/legin:v0.1 by /u/rakoo',
+				clientId: 'i4ji8frakYoSkw',
+				redirectUri: 'https://localhost:8000',
+			})
+
+			if (rr) {
+				window.localStorage.setItem('refreshToken', rr.refreshToken);
+				r = rr
+			}
+		} else {
+
+			// Case 2: we already have a refresh token
+			const refreshToken = window.localStorage.getItem('refreshToken')
+			if (refreshToken) {
+				const rr = new window.snoowrap({
+					userAgent: 'snoowrap:github.com/rakoo/legin:v0.1 by /u/rakoo',
+					clientId: 'i4ji8frakYoSkw',
+					clientSecret: '',
+					refreshToken: refreshToken
+				})
+
+				// Ping to make sure connection is on
+				await rr.getMe()
+				if (rr) {
+					r = rr
+				}
+			}
+		}
+
+		if (r) {
+			signedIn = true;
+			waiting = true
+			hot = await r.getHot();
+			waiting = false;
+		}
 	})
 
-	function setApiKey() {
-		if (!apiKeyInput.files) return;
-		let file = apiKeyInput.files[0];
-		if (!file) return;
-		let reader = new FileReader();
-		reader.onload = (evt) => {
-			apiKey = evt.target.result.trim();
-		}
-		reader.readAsText(file, 'UTF-8');
-	}
-
-	function setClientId() {
-		if (!clientIdInput.files) return;
-		let file = clientIdInput.files[0];
-		if (!file) return;
-		let reader = new FileReader();
-		reader.onload = (evt) => {
-			clientId = JSON.parse(evt.target.result).web.client_id
-		}
-		reader.readAsText(file, 'UTF-8');
-	}
-
-
-	/**
-	 *  On load, called to load the auth2 library and API client library.
-	 */
-	function handleClientLoad() {
-		gapi.load('client:auth2', handleAuth);
-	}
-
-	/**
-	 *  Initializes the API client library and sets up sign-in state
-	 *  listeners.
-	 */
-	async function handleAuth() {
-		// Array of API discovery doc URLs for APIs used by the quickstart
-		var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
-
-		// Authorization scopes required by the API; multiple scopes can be
-		// included, separated by spaces.
-		var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
-
-		if (!apiKey || !clientId) {
-			return;
-		}
-
-		gapi.client.init({
-			apiKey: apiKey,
-			clientId: clientId,
-			discoveryDocs: DISCOVERY_DOCS,
-			scope: SCOPES
-		}).then(() => {
-			// Listen for sign-in state changes.
-			gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-			// Handle the initial sign-in state.
-			gapi.auth2.getAuthInstance().signIn();
-			updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-		}, (error) => {
-			console.log(JSON.stringify(error, null, 2));
+	// Start the OAuth flow
+	async function login() {
+		const authUrl = window.snoowrap.getAuthUrl({
+			clientId: 'i4ji8frakYoSkw',
+			scope: ['read', 'submit', 'vote'],
+			redirectUri: 'https://localhost:8000',
+			state: 'foobar'
 		});
-	}
-
-	/**
-	 *  Called when the signed in status changes, to update the UI
-	 *  appropriately. After a sign-in, the API is called.
-	 */
-	function updateSigninStatus(isSignedIn) {
-		if (isSignedIn) {
-			signedIn = true;
-		} else {
-			signedIn = false;
-		}
-	}
-
-	/**
-	 *  Sign out the user upon button click.
-	 */
-	function handleSignoutClick(event) {
-		gapi.auth2.getAuthInstance().signOut();
+		window.location = authUrl
 	}
 
 </script>
 
 <main>
-	<div id="gapi" style="display: none;"></div>
-	<h1>LMail</h1>
-
-	{#if signedIn}
-		<button on:click={handleSignoutClick}>Sign Out</button>
-		<Content gapi={gapi} signedIn={signedIn}/>
+	<h1>Leĝin</h1>
+	{#if !signedIn}
+		<button on:click={login}>Login</button>
+	{:else if waiting}
+		<h2>Loading...</h2>
 	{:else}
+		<ul>
+			{#each hot as p, i}
+				<li>{p.title}</li>
+			{/each}
 
-		<table border="1">
-			<tbody>
-				<tr>
-					<th>API_KEY</th>
-					<td><input type="file" bind:this={apiKeyInput} on:change={setApiKey}></td>
-				</tr>
-				<tr>
-					<th>credentials.json</th>
-					<td><input type="file" bind:this={clientIdInput} on:change={setClientId}></td>
-				</tr>
-				<tr>
-					<th>Log in</th>
-					<td><button on:click={handleAuth}>Log In</button></td>
-				</tr>
-			</tbody>
-		</table>
+		</ul>
 	{/if}
 </main>
