9c80486b80265873f93662715ccdaa6953c1ec54
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2020-05-16 23:56:36 +0200

    Final UX adjustments
    - Make titlebar text vertically centered
    - Add author bottom right of cell
    - Make subreddit bold

--- src/Content.svelte
+++ src/Content.svelte
@@ -5,7 +5,7 @@
 	}
 
 	.item {
-		height: 160px;
+		height: 260px;
 		width: 400px;
 
 		margin-top: 10px;
@@ -15,20 +15,43 @@
 		font-family: monospace;
 		font-size: 13px;
 		background-color: #111;
-		border: 10px double #87CBD4;
-
-		grid-row-start: 2;
-		grid-row-end: 3;
-		grid-column-start: 1;
-		grid-column-end: 2;		
+		border: 3px solid #87CBD4;
+		border-radius: 3px;
 
 		display: grid;
-		grid-template-columns: 150px auto;
-		align-items: center;
+		grid-template-columns: auto;
 	}
 
 	.item > * {
+		margin: 3px;
+	}
+
+	.titlebar {
+		font-weight: bold;
+		margin-top: 10px;
+	}
+
+	.content {
+		height: 220px;
+		display: grid;
+		grid-template-columns: 150px auto;
+		align-items: center;
+		border: 1px solid #87CBD4;
+	}
+
+	.content > a {
+		grid-column-start: 1;
+		grid-column-end: 2;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+
+	.content > * {
 		margin: 10px;
+	}
+
+	.author {
+		text-align: right;
 	}
 
 </style>
@@ -42,20 +65,28 @@
 
 	onMount(async () => {
 		hot = await r.getHot();
-		console.log(hot)
 	})
 
+	function titleWithAuthor(post) {
+		return post.title + "\n\n" + post.author.name
+	}
+
+	function thumbnailOrLink(post) {
+		return post.thumbnail == "self" || post.thumbnail == "default" || post.thumbnail == "image" ? "" : post.thumbnail
+	}
 </script>
 
 <div class="flex-container">
 	{#each hot as p, i}
 		<div class="item">
-			<div>
+			<div class=titlebar>/{p.subreddit_name_prefixed}/</div>
+			<div class=content>
 				<a href={p.url}>
-					<img src={p.thumbnail || ""} height={p.thumbnailHeight} width={p.thumbnailWidth} alt="">
+					<img src={thumbnailOrLink(p)} height={p.thumbnailHeight} width={p.thumbnailWidth} alt="">
 				</a>
+				<p>{p.title}</p>
+				<p class="author">/u/{p.author.name}</p>
 			</div>
-			<p>{p.title}</p>
 		</div>
 	{/each}
 </div>
